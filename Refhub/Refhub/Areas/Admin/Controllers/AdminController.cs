c2bcec91f23c15f213941c40a2f6a28dd9a11c58
MohammadAgl <p.mohammad.agl@gmail.com>
2025-04-21 19:37:38 +0330

    "Add Areas"

---
+++ Refhub/Refhub/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Refhub.Areas.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
